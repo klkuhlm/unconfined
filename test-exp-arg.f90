b1bfc180a76d757c0303a7339c1517492b92b4e4
klkuhlm <kristopher.kuhlman@gmail.com>
2011-02-20 16:24:01 -0700

    more work on understanding denormalization and representation for exp/sinh/cosh

--- test-exp-arg.f90
+++ test-exp-arg.f90
@@ -10,10 +10,17 @@ program testexp
   !! full quad precision (only on gfortran >= 4.6 and ifort)
   integer, parameter :: QP = selected_real_kind(p=33,r=3000)
 
+  real(DP), parameter :: LOG2DP = log10(2.0_DP)
+  real(EP), parameter :: LOG2EP = log10(2.0_EP)
+  real(QP), parameter :: LOG2QP = log10(2.0_QP)
+
   ! complex should behave like reals of same kind
-  real(DP) :: x !,y,z
-  real(EP) :: i !,j,k
-  real(QP) :: a !,b,c
+  real(DP) :: x,y!,z
+  real(EP) :: i,j!,k
+  real(QP) :: a,b!,c
+
+  integer :: ii
+  character(38) :: fmt
 
   ! look at the numerical behavior of cosh/sinh/exp for large
   ! arguments.  Need to figure out the optimum/appropirate
@@ -49,6 +56,7 @@ program testexp
   ! happen for a given real/complex datatype (single-,double-,
   ! quad-precision).
 
+  fmt = '(I3,1X,ES22.15,2(1X,I3),2(1X,ES22.15))   '
   x = 1.0; i = 1.0; a = 1.0
   print *, '-log(epsilon(1.0_DP))',-log(epsilon(x)) ! ~36.04
   print *, 'digits(1.0_DP)       ',digits(x) ! 2^53
@@ -59,6 +67,12 @@ program testexp
   print *, '10**(1-precision(x))',10.0_DP**(1-(digits(x)-1)*log10(2.0_DP)) ! _not_ same as epsilon
   print *, 'log10(2.0)',log10(2.0_DP)
   print *, 'huge(x)',huge(x),1.0_EP-2.0_DP**(-digits(x))
+  do ii = 0,precision(x)+2
+     y = 1.0_DP - (1.0_DP + 10.0_DP**(-ii))
+     write(*,fmt) ii,y,-exponent(y),-int(exponent(y)*LOG2EP),fraction(1.0_DP),fraction((1.0_DP + 10.0_DP**(-ii)))
+  end do
+  
+  fmt = '(I3,1X,ES25.18,2(1X,I3),2(1X,ES25.18))               '
   print *, ' '
   print *, '-log(epsilon(1.0_EP))',-log(epsilon(i)) ! ~43.67
   print *, 'digits(1.0_EP)       ',digits(i) ! 2^64
@@ -69,6 +83,12 @@ program testexp
   print *, '10**(1-precision(x))',10.0_EP**(1-precision(i))
   print *, 'log10(2.0)',log10(2.0_EP)
   print *, 'huge(x)',huge(i),1.0_EP-2.0_EP**(-digits(i))
+  do ii=0,precision(i)+2
+     j = 1.0_EP - (1.0_EP + 10.0_EP**(-ii))
+     write(*,fmt) ii,j,-exponent(j),-int(exponent(j)*LOG2EP),fraction(1.0_EP),fraction((1.0_EP + 10.0_EP**(-ii)))
+  end do
+
+  fmt = '(I3,1X,ES40.33,2(1X,I3),2(1X,ES40.33))               '
   print *, ' '
   print *, '-log(epsilon(1.0_QP))',-log(epsilon(a)) ! ~77.63
   print *, 'digits(1.0_QP)       ',digits(a) ! 2^113
@@ -79,5 +99,9 @@ program testexp
   print *, '10**(1-precision(x))',10.0_QP**(1-precision(a))
   print *, 'log10(2.0)',log10(2.0_QP)
   print *, 'huge(x)',huge(a),1.0_QP-2.0_QP**(-digits(a))
+  do ii=0,precision(a)+2
+     b = 1.0_QP - (1.0_QP + 10.0_QP**(-ii))
+     write(*,fmt) ii,b,-exponent(b),-int(exponent(b)*LOG2QP),fraction(1.0_QP),fraction((1.0_QP + 10.0_QP**(-ii)))
+  end do
 
 end program testexp
