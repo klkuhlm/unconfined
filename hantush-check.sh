5a797115c4a0988fb606ca4cbd5810d5437b9949
klkuhlm <kristopher.kuhlman@gmail.com>
2011-07-15 12:41:03 -0600

    removed files replaced by python script

--- hantush-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for z in {0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0}; do
-    sed -e "s/%%%%z%%%%/${z}/g" < hantush-input.tpl > hantush-input.dat
-    echo "${z}"
-    ./unconfined ./hantush-input.dat 
-done
