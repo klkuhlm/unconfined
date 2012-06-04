7eec3d12ef774443e561d092197d2d566022664a
Kris Kuhlman <kristopher.kuhlman@gmail.com>
2012-06-03 22:08:49 -0600

    GI data: caught a few typos with checking script, fixed in Google doc and re-exported

--- grand-island/check-and-plot-GI-data.py
+++ grand-island/check-and-plot-GI-data.py
@@ -19,18 +19,18 @@ for file in files:
 
     fig = plt.figure(1)
     ax = fig.add_subplot(2,1,1)
-    ax.loglog(dt[pm]-t0,dd[pm],'r-')
+    ax.loglog((dt[pm]-t0)*1440,dd[pm],'r-')
     ax.set_ylabel('drawdown (ft)')
     ax2 = ax.twinx()
-    ax2.semilogx(dt[pm][1:]-t0, (dt[pm][1:]-dt[pm][:-1])*1440,'ro')
+    ax2.semilogx((dt[pm][1:]-t0)*1440, (dt[pm][1:]-dt[pm][:-1])*1440,'ro')
     ax2.set_ylabel('$\\Delta t$ (min)')
     ax.set_xlabel('time since pumping began (min)')
 
     ax = fig.add_subplot(2,1,2)
-    ax.loglog(dt[rm]-t1,dd[rm],'g-')
+    ax.loglog((dt[rm]-t1)*1440,dd[rm],'g-')
     ax.set_ylabel('drawdown (ft)')
     ax2 = ax.twinx()
-    ax2.semilogx(dt[rm][1:]-t1, (dt[rm][1:]-dt[rm][:-1])*1440,'ko')
+    ax2.semilogx((dt[rm][1:]-t1)*1440, (dt[rm][1:]-dt[rm][:-1])*1440,'ko')
     ax2.set_ylabel('$\\Delta t$ (min)')
     ax.set_xlabel('time since recovery began (min)')
 
