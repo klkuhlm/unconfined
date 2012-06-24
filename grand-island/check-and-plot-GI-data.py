8bbd5878b8a32b2eef843733dcf9cafe337e14f2
Kris Kuhlman <kristopher.kuhlman@gmail.com>
2012-06-24 12:32:40 -0600

    updated readme and plotting script

--- grand-island/check-and-plot-GI-data.py
+++ grand-island/check-and-plot-GI-data.py
@@ -9,7 +9,7 @@ t0 = pylab.datestr2num('07/29/1931 06:05:00')
 t1 = pylab.datestr2num('07/31/1931 06:04:00')
 
 for file in files:
-    print file.lstrip('grand-island-test-wenzel')
+    print file.lstrip('grand-island-test-wenzel - ').rstrip('.csv')
     dt,dd = np.loadtxt(file,delimiter=',',skiprows=1,
                        usecols=(2,5),unpack=True,
                        converters={2:pylab.datestr2num})
@@ -25,14 +25,6 @@ for file in files:
     ax2.semilogx((dt[pm][1:]-t0)*1440, (dt[pm][1:]-dt[pm][:-1])*1440,'ro')
     ax2.set_ylabel('$\\Delta t$ (min)')
     ax.set_xlabel('time since pumping began (min)')
-
-    ax = fig.add_subplot(2,1,2)
-    ax.loglog((dt[rm]-t1)*1440,dd[rm],'g-')
-    ax.set_ylabel('drawdown (ft)')
-    ax2 = ax.twinx()
-    ax2.semilogx((dt[rm][1:]-t1)*1440, (dt[rm][1:]-dt[rm][:-1])*1440,'ko')
-    ax2.set_ylabel('$\\Delta t$ (min)')
-    ax.set_xlabel('time since recovery began (min)')
 
     plt.savefig(file.replace('csv','png'))
     plt.close(1)
