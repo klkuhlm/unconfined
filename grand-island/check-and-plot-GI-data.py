fixed info and script to handle new filenames, added spline-based derivative calc

grand-island/check-and-plot-GI-data.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
 import matplotlib.mlab as mlab
+from scipy.interpolate import UnivariateSpline as spline 
 from glob import glob
 
-files = sorted(glob("grand-island-test-wenzel - *.csv"))
+files = sorted(glob("grand-island-test-wenzel-*.csv"))
 
 t0 = pylab.datestr2num('07/29/1931 06:05:00')
 t1 = pylab.datestr2num('07/31/1931 06:04:00')
@@ -16,8 +17,9 @@ tlen = (t1-t0)*1440
 # column 2: drawdown (ft)
 
 individualplots = True
+computesplinederiv = True
 drawdowncheck = True
-mapcheckplot = False
+mapcheckplot = True
 
 if drawdowncheck:
     if not individualplots:
@@ -30,7 +32,7 @@ if drawdowncheck:
     
         if 'info' not in filename:
     
-            well = filename.lstrip('grand-island-test-wenzel - ').rstrip('.csv') 
+            well = filename.lstrip('grand-island-test-wenzel-').rstrip('.csv') 
             print well
     
             t = []
@@ -52,15 +54,38 @@ if drawdowncheck:
             rm = dt > t1
         
             if individualplots:
+
+                tv = (dt[pm]-t0)*1440
+                dv = dd[pm]
+
                 fig = plt.figure(1)
                 ax = fig.add_subplot(1,1,1)
-                ax.loglog((dt[pm]-t0)*1440,dd[pm],'r-')
-                ax.loglog((dt[pm]-t0)*1440,dd[pm],'k.')
+                ax.loglog(tv,dv,'r-')
+                ax.loglog(tv,dv,'k.')
+
+                if computesplinederiv:
+                    ppm = np.logical_and(dt > t0,dt < t1)
+                    tpv = (dt[ppm]-t0)*1440
+                    dpv = dd[ppm]
+
+                    nd = dpv.shape[0]
+                    factor = nd*70
+                    sp = spline(np.log(tpv),dpv,s=np.linalg.norm(dv)/factor)
+                    
+                    spval = np.empty((nd,2))
+                    for j in range(nd):
+                        spval[j,:] = sp.derivatives(np.log(tpv[j]))[0:2]
+                    axd = ax.twinx()
+                    ax.semilogx(tpv,spval[:,0],'b--')
+                    axd.semilogx(tpv,spval[:,1],'g--')
+                    axd.set_ylabel('d s/d(ln(t)) (pumping only)')
+
                 ax.axvline(tlen)
                 ax.set_ylabel('drawdown (ft)')
-                ax2 = ax.twinx()
-                ax2.semilogx((dt[pm][1:]-t0)*1440, (dt[pm][1:]-dt[pm][:-1])*1440,'rx')
-                ax2.set_ylabel('$\\Delta t$ (min)')
+                if not computesplinederiv:
+                    ax2 = ax.twinx()
+                    ax2.semilogx(tv[1:], tv[1:]-tv[:-1],'rx')
+                    ax2.set_ylabel('$\\Delta t$ (min)')
                 ax.set_xlabel('time since pumping began (min)')
                 ax.set_title(well)
             
@@ -114,7 +139,7 @@ if mapcheckplot:
     # for creating maps for checking, etc.
     xydt = np.dtype([('id','S2'),('x','f8'),('y','f8')])
 
-    d = np.genfromtxt('grand-island-test-wenzel - info.csv',
+    d = np.genfromtxt('grand-island-test-wenzel-info.csv',
                       dtype=ddt,delimiter=',',
                       usecols=(0,1,3,4,5,6,7,8),skiprows=1,
                       filling_values=np.NaN)
@@ -126,17 +151,17 @@ if mapcheckplot:
     for well in d['id']:
         thiswell = xy['id'] == well
         lin = d[thiswell]['line'][0]
-        if lin == 'SW':
-            # the ray comes from 84, not 83. Radial distance -> 83 is
-            # correct, but XY computed is not
-            pass
-        else:
-            ang = angles[lin]*conv
-            r = d[thiswell]['r'][0]
-            x = r*np.cos(ang)
-            y = r*np.sin(ang)
-            xy['x'][thiswell] = x
-            xy['y'][thiswell] = y
+        #if lin == 'SW':
+        #    # the ray comes from 84, not 83. Radial distance -> 83 is
+        #    # correct, but XY computed is not
+        #    pass
+        #else:
+        ang = angles[lin]*conv
+        r = d[thiswell]['r'][0]
+        x = r*np.cos(ang)
+        y = r*np.sin(ang)
+        xy['x'][thiswell] = x
+        xy['y'][thiswell] = y
 
     buf = 50
     nx,ny = (40,40)