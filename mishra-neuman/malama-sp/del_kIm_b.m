661b9b6618247ded710aaba4cd3732ccd1e21533
Kris Kuhlman <kristopher.kuhlman@gmail.com>
2014-02-15 22:26:25 -0700

    updating Bwalya's matlab scripts to work in octave

--- mishra-neuman/malama-sp/del_kIm_b.m
+++ mishra-neuman/malama-sp/del_kIm_b.m
@@ -2,7 +2,7 @@ function u=del_kIm_b(k,rD,J0,p,zD,params)
 %x=0.0;
 %%n=k
 x = zeros(k,1);
-parfor m = 1:k+1
+for m = 1:k+1
     n=m-1;
     x(m) = ((-1)^n)*nchoosek(k,n)*integral_Ikb(k-n,rD,J0,p,zD,params);
 end
--- mishra-neuman/malama-sp/totalintegral_b.m
+++ mishra-neuman/malama-sp/totalintegral_b.m
@@ -1,7 +1,7 @@
 function u=totalintegral_b(N,M,p,rD,zD,J0,params)
 %x=0.0;
 x = zeros(M-N,1);
-parfor k=1:M+1
+for k=1:M+1
     n=k-1;
     x(k) = (((-1)^n)*del_kIm_b(n,rD,J0,p,zD,params)/(2^(n+1)));
 end
